27822dc587d3084f00fc0331c0ae5d403c4d0b79
ItaloFan <56197011+ItaloFan@users.noreply.github.com>
2023-05-15 19:09:50 +0800

    Sudoku Solver Version 1.2. Add version and git commit info.

--- src/main.cc
+++ src/main.cc
@@ -1,15 +1,28 @@
 #include <iostream>
 #include "solver.h"
+#include "SudokuSolverConfig.h"
+
+// 用于输出间隔符
+void dividing_line()
+{
+    std::cout << "------------------------------------------------------------" << std::endl;
+}
 
 int main(int argc, char const *argv[])
 {
     // 处理输入
     if (argc != 2 or std::string(argv[1]) == "-h" or std::string(argv[1]) == "--help")
     {
-        std::cout << "Usage: "
-                  << "./sudoku_solver.exe"
-                  << " <puzzle>" << std::endl;
-        std::cout << "Where <puzzle> is a string of size 81, representing the row-major Sudoku puzzle to be solved and \nincluding numbers only. ";
+        dividing_line();
+        std::cout << "SudokuSolver " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+        std::cout << "  (Commit ID: " << GIT_COMMIT_ID << " Commit Date: " << GIT_COMMIT_DATE << ")" << std::endl;
+        dividing_line();
+        std::cout << "Usage:\n "
+                  << "./sudoku_solver.exe <puzzle>"
+                  << std::endl;
+        dividing_line();
+        std::cout << "Where <puzzle> is a string of size 81, representing "
+                  << "the \nrow-major Sudoku puzzle to be solved and including numbers only. " << std::endl;
         std::cout << "'0' or '.' is used as a place holder. " << std::endl;
         return 0;
     }
